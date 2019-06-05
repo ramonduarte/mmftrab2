313e3b58d61f018e56e19a5f990112d7f0893507
Ramon Melo <ramonduarte@poli.ufrj.br>
2019-06-05 19:40:07 -0300

    Activity 1 ready. Activity 2 running.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 ## TODO: get to v0
 
 
-def alg(k=9):
+def ativ1(contract="call", k=9):
     N = 2**k
     u = 2
     s0 = 4
@@ -15,16 +15,52 @@ def alg(k=9):
     strike = 5
     p = 0.5
 
-    v_list = [s0*u**i for i in range(N//2, -N//2 - 1, -1)]
+    if (contract == "call"):
+        v_list = [max((s0*u**i-strike)/(1+r), 0) for i in range(N//2, -N//2 - 1, -1)]
+    else:
+        v_list = [max((strike-s0*u**i)/(1+r), 0) for i in range(N//2, -N//2 - 1, -1)]
+    
+    # for i in range(N//2, -N//2 - 1, -1):
+        # print ((k-s0*u**i)/(1+r))
+        # print(s0, u**i, 1+r)
+    # raise Exception
 
     while len(v_list) > 1:
-        vm1_list = [v_list[j]*p + v_list[j+1] for j in range(len(v_list)-1)]
+        vm1_list = [v_list[j]*p + v_list[j+1]*(1-p) for j in range(len(v_list)-1)]
         v_list = vm1_list[:]
 
     return v_list[0]
 
 
+def ativ2(contract="call", M=5000, k=9):
+    N = 2**k
+    u = 2
+    s0 = 4
+    r = 0.25
+    strike = 5
+    p = 0.5
+    v0 = 0
+
+    if (contract == "call"):
+        for m in range(M):
+            v0 = (v0*m + ativ1("call", i))/(m+1)
+    else:
+        for m in range(M):
+            v0 = (v0*m + ativ1("put", i))/(m+1)
+
+    return v0
+
+
+
+
 if __name__ == "__main__":
-    with open("filename.txt", "w") as filename:
+    # # with open("ativ1_put.txt", "w") as filename:
+    # with open("ativ1_call.txt", "w") as filename:
+    #     for i in range(1, 13):
+    #         # filename.write(str(i) + " -> " + str(ativ1("put",i)) + "\n")
+    #         filename.write(str(i) + " -> " + str(ativ1("call",i)) + "\n")
+
+    with open("ativ2.txt", "w") as filename:
         for i in range(1, 13):
-            filename.write(str(i) + " -> " + str(alg(i)) + "\n")
+            filename.write(str(i) + " -> " + str(ativ2("call", M=5000, k=i)) + " CALL\n")
+            filename.write(str(i) + " -> " + str(ativ2("put", M=5000, k=i)) + " PUT\n")
